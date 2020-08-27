ed4cd42b1908c1b42ffe90ab293d3e2e38fec258
Amy Amrani <vesamrani@gmail.com>
2020-08-26 22:16:55 -0400

    updated for production deployment

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
   // API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:9090'
-  API_BASE_URL: process.env.REACT_APP_API_BASE_URL || `http://localhost:8000`,
+  // API_BASE_URL: process.env.REACT_APP_API_BASE_URL || `http://localhost:8000`,
+  API_BASE_URL: process.env.REACT_APP_API_BASE_URL || `https://fathomless-falls-22348.herokuapp.com`,
 }
